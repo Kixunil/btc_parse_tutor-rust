261ff6bc2060223237061e6ea394b01c6b507daf
Martin Habovštiak <martin.habovstiak@gmail.com>
2017-12-08 18:48:28 +0100

    Chapter: Intro - Primitives - Varint

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,16 @@ use byteorder::{LE, ReadBytesExt};
 use std::io;
 use std::io::{Read, ErrorKind};
 
+/// Deserializes "varint" as defined by Bitcoin protocol.
+fn deserialize_varint<R: Read>(reader: &mut R) -> io::Result<u64> {
+    match reader.read_u8()? {
+        253 => reader.read_u16::<LE>().map(Into::into),
+        254 => reader.read_u32::<LE>().map(Into::into),
+        255 => reader.read_u64::<LE>().map(Into::into),
+        x   => Ok(x.into()),
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
