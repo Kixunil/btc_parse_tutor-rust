ff33ee81b1b02075612b295c730b76190741c54b
Martin Habovštiak <martin.habovstiak@gmail.com>
2017-12-09 01:10:16 +0100

    Chapter: Intro - Primitives - Script

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,27 @@ fn deserialize_varint<R: Read>(reader: &mut R) -> io::Result<u64> {
     }
 }
 
+/// Represent's Bitcoin script.
+struct Script(Vec<u8>);
+
+impl Script {
+    /// Deserializes the script from a reader.
+    fn deserialize<R: Read>(reader: &mut R) -> io::Result<Self> {
+        let len = deserialize_varint(reader)?;
+        // This is consensus rule, so theoretically no need to check it,
+        // but serves as a protection against corrupted inputs.
+        if len > 10_000 {
+            return Err(ErrorKind::InvalidData.into());
+        }
+
+        let mut reader = reader.by_ref().take(len);
+        let mut data = Vec::with_capacity(len as usize);
+
+        io::copy(&mut reader, &mut data)?;
+        Ok(Script(data))
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
